00f52876756312d681f9e87d564c7d5640f7cfbf
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2022-09-26 13:52:27 +0400

    first-work

---
+++ app.js
@@ -0,0 +1,13 @@
+import Express from "express"
+
+const app = Express()
+const port = 3000
+const blog = { id: 1, title: "Blog title", description: "Blog description" }
+
+app.get("/", (req, res) => {
+  res.send(blog)
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
