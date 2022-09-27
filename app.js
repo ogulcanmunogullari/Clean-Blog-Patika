5646ed9fee17817668039dc9fdb958ca7a7d8cad
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2022-09-27 16:58:46 +0400

    mongodb post get

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import express from "express"
+import Post from "./models/Post.js"
+import mongoose from "mongoose"
 
 const app = express()
+//connect to mongodb
+mongoose.connect("mongodb://localhost/cleanblog-db")
+//template engine
 app.set("view engine", "ejs")
 
 //Middleware
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 //Routes
-app.get("/", (req, res) => {
-  res.render("index")
+app.get("/", async (req, res) => {
+  const posts = await Post.find({})
+  res.render("index", { posts })
 })
 app.get("/about", (req, res) => {
   res.render("about")
@@ -18,6 +26,11 @@ app.get("/post", (req, res) => {
 })
 app.get("/add-post", (req, res) => {
   res.render("add_post")
+})
+
+app.post("/take-post", async (req, res) => {
+  await Post.create(req.body)
+  res.redirect("/")
 })
 
 const port = 3000
