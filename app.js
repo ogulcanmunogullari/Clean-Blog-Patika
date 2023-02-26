7a775743c115148cb5550d6e4fb84e881fc1ac2e
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2023-02-26 19:09:53 +0300

    changing database connection / local to cloud

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-import express from "express"
-import Post from "./models/Post.js"
-import mongoose from "mongoose"
+import express from 'express';
+import Post from './models/Post.js';
+import mongoose from 'mongoose';
 
-const app = express()
+const app = express();
 //connect to mongodb
-mongoose.connect("mongodb://localhost/cleanblog-db")
+mongoose.connect(
+  'mongodb+srv://ogulcan:12qwaszx@database.xjxvdni.mongodb.net/cleanblogdb?retryWrites=true&w=majority',
+);
+
 //template engine
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs');
 
 //Middleware
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
-app.get("/", async (req, res) => {
-  const posts = await Post.find({})
-  res.render("index", { posts })
-})
-app.get("/about", (req, res) => {
-  res.render("about")
-})
-app.get("/post", (req, res) => {
-  res.render("post")
-})
-app.get("/add-post", (req, res) => {
-  res.render("add_post")
-})
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', { posts });
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+app.get('/add-post', (req, res) => {
+  res.render('add_post');
+});
 
-app.post("/take-post", async (req, res) => {
-  await Post.create(req.body)
-  res.redirect("/")
-})
+app.post('/take-post', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
 
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
